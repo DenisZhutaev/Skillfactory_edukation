0e8d8522b0a5658716c12cd54face330f9a1b0c6
Erty <63260683+DenisZhutaev@users.noreply.github.com>
2023-04-02 17:02:48 +0300

    Добавление документации классов

--- Task_16.8.2/rectangle.py
+++ Task_16.8.2/rectangle.py
@@ -1,25 +1,44 @@
 class Rectangle:
+    """Класс прямоугольник
+    Параметры:
+    a (int or float): первая сторона прямоугольника
+    b (int or float): вторая сторона прямоугольника
+    """
+
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
     def get_area_rectangle(self):
+        """Метод возвращвет площадь прямоугольника"""
         return self.a * self.b
 
 
 class Square:
+    """Класс квадрат
+       Параметры:
+       a (int or float): у квадрата все стороны равны
+    """
 
     def __init__(self, a):
         self.a = a
 
     def get_area_sqare(self):
+        """Метод возвращвет площадь квадрата"""
         return self.a ** 2
 
 
 class Circle:
+    """Класс квадрат
+           Параметры:
+           a (int or float): радиус круга
+           число Пи (float): 3.14
 
+    Формула нахождения площади круга: S=πR²
+    """
     def __init__(self, a):
         self.a = a
 
     def get_area_circle(self):
+        """Метод возвращвет площадь круга"""
         return 3.14 * self.a ** 2
