872d6648b9504515f010fc2e5fd49f774693d44c
Erty <63260683+DenisZhutaev@users.noreply.github.com>
2023-04-22 17:38:08 +0300

    Telegram bot который показывает курс обмена валюты одной на другую, написан с помощью сайта https://www.cryptocompare.com/

---
+++ Task_18.6.1_currency_bot/config.py
@@ -0,0 +1,9 @@
+TOKEN = '5640107001:AAETbZtZyHaeeTzeRe4l4Q6bAOlTkZJaqis'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ Task_18.6.1_currency_bot/extensions.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
