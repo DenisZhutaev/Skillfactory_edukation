bd67eec501b747cd7cdf905b3a4ce3a6fef64e91
Erty <63260683+DenisZhutaev@users.noreply.github.com>
2023-06-01 17:46:11 +0300

    добавление заданий в репозиторий

---
+++ test_24_4.py
@@ -0,0 +1,38 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_search_example(selenium):
+    """ Search some phrase in google and make a screenshot of the page. """
+
+    # Open google search page:
+    selenium.get('https://google.com')
+
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Find the field for search text input:
+    search_input = selenium.find_element(By.CLASS_NAME, 'gLFyf')
+
+    # Enter the text for search:
+    search_input.clear()
+    search_input.send_keys('first test')
+
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Click Search:
+    search_button = selenium.find_element(By.CLASS_NAME, 'gNO89b')
+    search_button.submit()
+
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
+
+    # Make the screenshot of browser window:
+    selenium.save_screenshot('result.png')
+
+
+
+# так запустятся все файды в папке
+# python3 -m pytest -v --driver Chrome --driver-path /Users/deniszutaev/PycharmProjects/Skillfactory/test_24_4.py
+
+
+# вот так верно запускть конкретный файд
+# pytest -v --driver Chrome --driver-path driver-path/chromedriver.exe test_24_4.py
